11d9ef21adf7827340e63e1285a670771865c513
Aftab Jamil <aftab.jamil@pacteraedge.com>
2021-07-28 18:22:27 +0530

    Added different laoyout

---
+++ VApp/Controllers/AffectedController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VApp.Controllers
+{
+    public class AffectedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Insert()
+        {
+            return View();
+        }
+    }
+}
