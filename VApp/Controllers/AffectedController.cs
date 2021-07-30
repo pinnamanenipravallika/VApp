332d7558614309ce910a168857a438f3ee809c04
Aftab Jamil <aftab.jamil@pacteraedge.com>
2021-07-30 09:58:06 +0530

    Added affected form

--- VApp/Controllers/AffectedController.cs
+++ VApp/Controllers/AffectedController.cs
@@ -1,14 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using VApp.Entities;
+using VApp.Models;
 
 namespace VApp.Controllers
 {
     public class AffectedController : Controller
     {
+        private readonly VaccinationdbContext _db;
+
+        public AffectedController(VaccinationdbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.EmpId = 1589;
             return View();
         }
-        public IActionResult Insert()
+        [HttpPost]
+        public IActionResult InsertAffectedEmployee(AffectedModel affectedModel)
+        {
+            var data = new AffectedEmployee()
+            {
+                EmpId = affectedModel.EmpId,
+                IsRecoveryed = affectedModel.IsRecoveryed,
+                RecoveryDuration = affectedModel.RecoveryDuration
+            };
+            var dataExist = _db.AffectedEmployees.FirstOrDefault(a => a.EmpId == affectedModel.EmpId);
+            if (dataExist == null)
+            {
+                _db.AffectedEmployees.Add(data);
+                _db.SaveChanges();
+            }
+            else
+            {
+                dataExist.IsRecoveryed = affectedModel.IsRecoveryed;
+                dataExist.RecoveryDuration = affectedModel.RecoveryDuration;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Dashboard", "Login");
+        }
+
+        public IActionResult AffectedFamily()
         {
             return View();
         }
---
+++ VApp/Models/AffectedFamilyModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace VApp.Models
+{
+    public class AffectedFamilyModel
+    {
+        public int EmpId { get; set; }
+        public string MemberName { get; set; }
+        public bool IsRecoveryed { get; set; }
+        public int? RecoveryDuration { get; set; }
+        public int? RelationshipId { get; set; }
+        public List<RelationshipTypeModel> RelationshipTypes { get; set; } = new List<RelationshipTypeModel>();
+    }
+    public class RelationshipTypeModel
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+}
