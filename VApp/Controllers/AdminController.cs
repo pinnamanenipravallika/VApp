c6948ff92d5cb64644dc9749e5ef12cd7e0cb992
Jaipal <jaipalreddy.sanivarapu@pacteraedge.com>
2021-07-30 10:32:11 +0530

    Admin Module with simple search

---
+++ VApp/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VApp;
+using VApp.Models;
+using Microsoft.EntityFrameworkCore;
+using VApp.Entities;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+
+namespace VApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly VaccinationdbContext _db;
+
+        public AdminController(VaccinationdbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            
+            var displaydata = _db.Employees.ToList();
+            return View(displaydata);
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult> Index(string Empsearch)
+        {
+            ViewData["GetEmployeeDetails"] = Empsearch;
+            var emp = from i in _db.Employees select i;
+            if(!String.IsNullOrEmpty(Empsearch))
+            {
+                emp = emp.Where(i => i.FirstName.Contains(Empsearch) || i.Email.Contains(Empsearch) || i.Code.Contains(Empsearch));
+            }
+
+            return View(await emp.AsNoTracking().ToListAsync());
+        }
+
+
+    }
+}
---
+++ VApp/Models/AdminModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using VApp.Entities;
+
+namespace VApp.Models
+{
+    public class AdminModel
+    {
+       
+        public string EmployeeId { get; set; }
+       
+        public string EmployeeName { get; set; }
+
+        public string Designation { get; set; }
+
+        public string Email { get; set; }
+        
+        public string MobileNumber { get; set; }
+
+        public string Address { get; set; }
+        
+        public string IsVaccinated { get; set; }
+        
+        public string IsAffected { get; set; }
+        
+        public string RecoveryDate { get; set; }
+        
+        public int RecoveryDuration { get; set; }
+        
+        public string VaccineName { get; set; }       
+
+        public string VaccinationDateOne { get; set; }
+    }
+}
