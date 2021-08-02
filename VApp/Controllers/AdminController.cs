c6645eb4302fffb2dc54a3f800123a5554b0f9e6
Syeda <syedamariya.tanzil@pacteraedge.com>
2021-08-02 13:41:59 +0530

    Removed Entitty and Started using admin model

--- VApp/Controllers/AdminController.cs
+++ VApp/Controllers/AdminController.cs
@@ -23,47 +23,102 @@ namespace VApp.Controllers
         }
         public IActionResult Index()
         {
-            
-            var displaydata = _db.Employees.ToList();
+            var model = new AdminModel();
+            var getEmployeeDetails = _db.Employees.ToList();
 
-            return View(displaydata);
+            List<EmployeeDataModel> details = new List<EmployeeDataModel>();
+
+                    for (int i = 0; i < getEmployeeDetails.Count; i++)
+                    {
+                        var empDash = new EmployeeDataModel
+                        {
+                            ID = getEmployeeDetails[i].Id,
+                            Code = getEmployeeDetails[i].Code,
+                            FirstName = getEmployeeDetails[i].FirstName,
+                            LastName = getEmployeeDetails[i].LastName,
+                            Email = getEmployeeDetails[i].Email,
+                            Mobile = getEmployeeDetails[i].Mobile,
+                            Address = getEmployeeDetails[i].Address,
+                            JoiningDate = getEmployeeDetails[i].JoiningDate
+                        };
+                         
+
+                         var vaccinationDetails = _db.VaccinationDetails.Where(v => v.EmpId == empDash.ID)
+                        .Join(_db.VaccinationNames, vd => vd.VccineNameId, vn => vn.Id, (vd, vn) => new { vd, vn })
+                        .Join(_db.DoseTypes, vd => vd.vd.DoseTypeId, dt => dt.Id, (vd, dt) => new VaccineListDataModel
+                        {
+                            VaccinationDetails = vd.vd,
+                            VaccinationNames = vd.vn,
+                            DoseTypes = dt
+                        }).ToList();
+
+
+                                List<EmployeeVaccinationDataModel> d = new List<EmployeeVaccinationDataModel>();
+
+                                for (int j = 0; j < vaccinationDetails.Count; j++)
+                                {
+
+                                    EmployeeVaccinationDataModel empvdetails = new EmployeeVaccinationDataModel
+                                    {
+
+                                        VccineName = vaccinationDetails[j].VaccinationNames.Name,
+                                        DoseType = vaccinationDetails[j].DoseTypes.DoseType1,
+                                        VaccinationDate = vaccinationDetails[j].VaccinationDetails.VaccinationDate,
+                                        HospitalName = vaccinationDetails[j].VaccinationDetails.HospitalName,
+                                        CertificatePath = vaccinationDetails[j].VaccinationDetails.CertificatePath
+                                    };
+
+                                    d.Add(empvdetails);
+                                }
+
+                         details.Add(empDash);
+                     model.EmpVaccinationData = d;
+                     model.EmpData = details;
+                    }
+            return View(model);
         }
+       
+     }
 
-        [HttpGet]
+        //[HttpGet]
 
-        public async Task<ActionResult> Index(string Empsearch)
-        {
-            ViewData["GetEmployeeDetails"] = Empsearch;
-            var emp = from i in _db.Employees select i;
-            if(!String.IsNullOrEmpty(Empsearch))
-            {
-                emp = emp.Where(i => i.FirstName.Contains(Empsearch) || i.Email.Contains(Empsearch) || i.Code.Contains(Empsearch));
-            }
-
-            return View(await emp.AsNoTracking().ToListAsync());
-        }
-        [HttpPost]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var deleteRecord = await _db.Employees.FindAsync(id);
-            if (deleteRecord == null)
-            {
-                return NotFound();
-
-            }
-            _db.Employees.Remove(deleteRecord);
-            await _db.SaveChangesAsync();
-
-            return RedirectToAction("Index","Admin");
-        }
-        [HttpPost]
-        public async Task<IActionResult> Update(EmployeeDataModel employee)
-        {
+        //public async Task<ActionResult> Index(string Empsearch)
+        //{
 
 
-            return RedirectToAction("Index", "Admin");
+        //        ViewData["GetEmployeeDetails"] = Empsearch;
+        //        var emp = from i in _db.Employees select i;
+        //        if (!String.IsNullOrEmpty(Empsearch))
+        //        {
+        //            emp = emp.Where(i => i.FirstName.Contains(Empsearch) || i.Email.Contains(Empsearch) || i.Code.Contains(Empsearch));
+        //        }
 
-        }
+        //        return View(await emp.AsNoTracking().ToListAsync());
+        //}
 
-    }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var deleteRecord = await _db.Employees.FindAsync(id);
+        //    if (deleteRecord == null)
+        //    {
+        //        return NotFound();
+
+        //    }
+        //    _db.Employees.Remove(deleteRecord);
+        //    await _db.SaveChangesAsync();
+
+        //    return RedirectToAction("Index","Admin");
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> Update(EmployeeDataModel employee)
+        //{
+
+
+        //    return RedirectToAction("Index", "Admin");
+
+        //}
+
+    //}
 }
