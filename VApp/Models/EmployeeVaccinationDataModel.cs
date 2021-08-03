2ef71e7533028597cd11ef7d443f773805a1e66a
tanmayipactera <tanmayiyuktha.nama@pacteraedge.com>
2021-08-03 15:55:10 +0530

    Changed the display attribute and UI of search page.

--- VApp/Models/EmployeeVaccinationDataModel.cs
+++ VApp/Models/EmployeeVaccinationDataModel.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace VApp.Models
 {
     public class EmployeeVaccinationDataModel
     {
-
+        [Display(Name = "VaccineName")]
         public string VccineName { get; set; }
         public int? DoseType { get; set; }
         public DateTime? VaccinationDate { get; set; }
